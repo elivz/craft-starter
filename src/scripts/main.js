8558c87260cf3a13f92d9948194301e01562fce9
Eli Van Zoeren <eli@elivz.com>
2019-05-24 13:30:08 -0400

    Update to Tailwind 1.0

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,9 +3,7 @@ import 'instant.page';
 import 'intersection-observer';
 import reframe from 'reframe.js/dist/reframe.es';
 import './components/header';
-import './lib/analytics';
 import './plugins/external-links';
-import './plugins/smooth-scroll';
 
 // Make sure videos keep their aspect ratio
 reframe('iframe:not(.no-reframe)');
--- src/scripts/plugins/smooth-scroll.js
+++
@@ -1,8 +0,0 @@
-import SweetScroll from 'sweet-scroll';
-
-// Initialize smooth-scroll
-new SweetScroll({
-  trigger: 'a[href^="#"]:not(.no-smooth-scroll)',
-  header: '#header',
-  updateURL: true,
-});
